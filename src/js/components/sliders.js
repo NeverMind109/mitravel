6785ea332ace99eb0e3165e2e96e4c065a03b10b
NeverMind109 <vladgladkih123@gmail.com>
2022-05-13 23:25:05 +0300

    Created: Added Burger Menu & Hero Slider

--- src/js/components/sliders.js
+++ src/js/components/sliders.js
@@ -3,17 +3,45 @@ import Swiper, { Navigation, Pagination } from "swiper";
 Swiper.use([Navigation, Pagination]);
 
 const featuredVideos = document.querySelector(".slider-container");
+const heroMedia = document.querySelector(".main-slider__swiper");
+const heroPlay = document.querySelectorAll(".main-slider__play");
+
+heroPlay.forEach((el) => {
+  el.addEventListener("click", (e) => {
+    let video = e.currentTarget
+      .closest(".main-slider__media")
+      .querySelector("video");
+    video.play();
+    e.currentTarget.style.display = "none";
+    setTimeout(() => {
+      video.volume = 0.5;
+    }, 1000);
+  });
+});
 
 const featuredVideosSlider = new Swiper(featuredVideos, {
   loop: true,
   centeredSlides: true,
   spaceBetween: 105,
   slidesPerView: auto,
+});
+
+const heroMediaSlider = new Swiper(heroMedia, {
+  loop: true,
+  spaceBetween: 0,
+  slidesPerView: 1,
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".hero-btn-right",
+    prevEl: ".hero-btn-left",
   },
-  scrollbar: {
-    el: ".swiper-scrollbar",
-  },
+});
+heroMediaSlider.on("transitionEnd", function () {
+  let videos = document.querySelectorAll(".hero__slider video");
+  videos.forEach((el) => {
+    el.pause();
+    el.currentTime = 0;
+  });
+  heroPlay.forEach((el) => {
+    el.style.display = "block";
+  });
 });
